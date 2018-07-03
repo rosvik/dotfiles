580cf16bf6819a0298a6d15787c5bffce8164ec8
Johannes Røsvik <j.rosvik@gmail.com>
2018-07-03 11:25:03 +0200

    Silent IP fetching

--- unix/bash_aliases_unix.sh
+++ unix/bash_aliases_unix.sh
@@ -2,7 +2,7 @@
 #    UNIX ALIASES
 # ------------------
 
-alias ip="curl icanhazip.com" # Your public IP address
+alias ip="curl -s icanhazip.com" # Your public IP address
 alias ldir="ls -la | grep ^d" # List all directories in current directory in long list format
 alias up="uptime" # Computer uptime
 alias py="python3" # Make using python less cancer
