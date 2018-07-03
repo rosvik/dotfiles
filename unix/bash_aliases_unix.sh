f9c9abce6be653801521008716ed5606bcff31e7
rosvik <j.rosvik@gmail.com>
2018-07-03 20:09:44 +0200

    Added reload alias and sublime tweaks

--- unix/bash_aliases_unix.sh
+++ unix/bash_aliases_unix.sh
@@ -6,6 +6,7 @@ alias ip="curl -s icanhazip.com" # Your public IP address
 alias ldir="ls -la | grep ^d" # List all directories in current directory in long list format
 alias up="uptime" # Computer uptime
 alias py="python3" # Make using python less cancer
+alias reload="exec bash -l"
 
 # Display connected drives
 alias disks="df -h"
