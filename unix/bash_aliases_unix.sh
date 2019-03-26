543f6df720f740e64fd39f0bc5a3b77867ebe47d
Johannes <j.rosvik@gmail.com>
2019-03-26 23:34:18 +0100

    Added cl alias

--- unix/bash_aliases_unix.sh
+++ unix/bash_aliases_unix.sh
@@ -4,6 +4,7 @@
 
 alias ip="curl -s icanhazip.com" # Your public IP address
 alias ldir="ls -la | grep ^d" # List all directories in current directory in long list format
+alias cl="clear;ls -la" # Clear screen then list the current directory
 alias up="uptime" # Computer uptime
 alias reload="exec bash -l"
 alias t="tree -CF --filelimit 25 --dirsfirst"
@@ -16,3 +17,5 @@ alias disk="df -h"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+
+alias ripme="java -jar ~/.dotfiles/unix/ripme.jar -l ."
