794b131bcca5153e5e6819e153b1e99cf7fed4e4
Johannes Røsvik <j.rosvik@gmail.com>
2024-10-14 23:41:11 +0200

    linux/bashrc: add pyenv config

--- linux/bashrc.sh
+++ linux/bashrc.sh
@@ -167,3 +167,8 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install pyenv with `curl https://pyenv.run | bash`
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
